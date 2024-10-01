1e56a3e7a489137ec1fa3361038814b9a2c589be
Vitor Hugo Coelho <vitor.hugo.cq@live.com>
2024-10-01 16:33:40 -0300

    build: removing legacy modules

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,15 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
-import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
-import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
-import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { SharedModule } from '../shared/shared.module';
@@ -17,17 +13,16 @@ import { CourseFormComponent } from './course-form/course-form.component';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
-@NgModule({ declarations: [CoursesComponent, CourseFormComponent], imports: [CommonModule,
-        CoursesRoutingModule,
-        SharedModule,
-        ReactiveFormsModule,
-        MatCardModule,
-        MatTableModule,
-        MatToolbarModule,
-        MatProgressSpinnerModule,
-        MatDialogModule,
-        MatButtonModule,
-        MatIconModule,
-        MatFormFieldModule,
-        MatInputModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [CoursesComponent, CourseFormComponent],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+})
 export class CoursesModule {}
