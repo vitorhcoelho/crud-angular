bca3af8fa894a7e2607f95c5b468e4568f7f2b10
Vitor Hugo Coelho <vitor.hugo.cq@live.com>
2024-10-01 15:03:29 -0300

    build: updating angular versions

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
 import { catchError, Observable, of } from 'rxjs';
 
 import { Course } from '../model/course';
